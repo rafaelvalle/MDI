b0462203c55699c7f04e6ef6c0c2231f49626cd6
Rafael Valle <jrafaelvalle@gmail.com>
2016-02-24 12:04:17 -0800

    adding python routines to create train and test data

---
+++ preprocess_data.py
@@ -0,0 +1,102 @@
+import numpy as np
+import cPickle as pkl
+import os
+from scipy import delete
+from sklearn.preprocessing import StandardScaler
+from missing_data_imputation import Imputer
+from processing import impute, perturbate_data
+from params import imp_methods, params_dict, scalers_folder
+from params import feats_train_folder, labels_train_folder, perturb_folder
+
+
+def set_trace():
+    from IPython.core.debugger import Pdb
+    import sys
+    Pdb(color_scheme='Linux').set_trace(sys._getframe().f_back)
+
+# load features and labels
+x = np.genfromtxt('data/adult-train-raw', delimiter=', ', dtype=object)
+
+# remove holland from data to match feature size in test data
+x = x[x[:, -2] != 'Holand-Netherlands']
+
+# binarize labels
+labels = (np.array(x[:, -1]) == '>50K').astype(int)
+labels = labels.reshape((-1, 1))
+
+# save labels in binary and one-hot representations
+labels.dump(os.path.join(labels_train_folder, 'labels_bin.np'))
+(np.eye(2)[labels]).astype(int).dump(os.path.join(labels_train_folder,
+                                                  'labels_bin_onehot.np'))
+
+# remove redundant education-number and labels features
+x = delete(x, (4, 14), 1)
+
+# enumerate parameters
+monotone = True
+ratios = np.arange(.1, .5, .1)
+
+for ratio in ratios:
+    print '\nPerturbing {}% of data'.format(ratio)
+    pert_data, _ = perturbate_data(x, params_dict['cat_cols'], ratio, monotone,
+                                   params_dict['miss_data_symbol'])
+
+    path = os.path.join(perturb_folder,
+                        'adult_train_pert_mono_{}_ratio_{}.csv'.format(monotone,
+                                                                       ratio))
+    # save perturbed data to disk as csv
+    print '\tSaving perturbed data to {}'.format(path)
+    np.savetxt(path, pert_data, delimiter=",", fmt="%s")
+
+    for imp_method in imp_methods:
+        print '\tImputing with {}'.format(imp_method)
+        imp = Imputer()
+        data = impute(x, imp, imp_method, params_dict)
+
+        path = "data/imputed/{}_mono_{}_ratio_{}.csv".format(imp_method,
+                                                             monotone,
+                                                             ratio)
+        # save data as csv
+        print '\tSaving imputed data to {}'.format(path)
+        np.savetxt(path, data, delimiter=",", fmt="%s")
+
+        # scale continuous variables and convert categorial to one-hot
+        # store the scaler objects to be used on the test set
+        scaler_path = os.path.join(scalers_folder,
+                               "{}_scaler".format(imp_method))
+
+        if os.path.isfile(scaler_path):
+            scaler_dict = pkl.load(open(scaler_path, "rb"))
+        else:
+            scaler_dict = {}
+
+        scaler = StandardScaler()
+        scaler = scaler.fit(data[:, params_dict['non_cat_cols']].astype(float))
+
+        data_scaled = np.copy(data)
+        data_scaled[:, params_dict['non_cat_cols']] = scaler.transform(
+            data[:, params_dict['non_cat_cols']].astype(float))
+
+        # key is imputation method and ratio dependent
+        # filename is imputation method dependent
+        scaler_dict["{}_ratio_{}".format(imp_method, ratio)] = scaler
+        pkl.dump(scaler_dict, open(scaler_path, 'wb'))
+
+
+        # binarize scaled data
+        data_scaled_bin = imp.binarize_data(data_scaled,
+                                            params_dict['cat_cols'],
+                                            params_dict['miss_data_symbol'])
+        # convert to float
+        data_scaled_bin = data_scaled_bin.astype(float)
+
+        # add labels as last column
+        data_scaled_bin = np.hstack((data_scaled_bin, labels))
+
+        # save to disk
+        filename = "{}_bin_scaled_mono_{}_ratio_{}.np".format(imp_method,
+                                                              monotone,
+                                                              ratio)
+        path = os.path.join(feats_train_folder, filename)
+        print '\tSaving imputed scaled and binarized data to {}'.format(path)
+        data_scaled_bin.dump(path)
---
+++ preprocess_test_data.py
@@ -0,0 +1,62 @@
+import numpy as np
+import cPickle as pickle
+import os
+from scipy import delete
+from missing_data_imputation import Imputer
+from processing import impute
+from params import imp_methods, params_dict, scalers_folder
+from params import feats_test_folder, labels_test_folder
+
+
+def set_trace():
+    from IPython.core.debugger import Pdb
+    import sys
+    Pdb(color_scheme='Linux').set_trace(sys._getframe().f_back)
+
+# load features and labels
+x = np.genfromtxt('data/adult-test-raw', delimiter=', ', dtype=object)
+
+# binarize labels
+labels = (np.array(x[:, -1]) == '>50K').astype(int)
+labels = labels.reshape((-1, 1))
+
+# save labels in binary and one-hot representations
+labels.dump(os.path.join(labels_test_folder, 'labels_bin_test.np'))
+(np.eye(2)[labels.astype(int)]).astype(int).dump(
+    os.path.join(labels_test_folder, 'labels_onehot_test.np'))
+
+# remove redundant education-number and labels features
+x = delete(x, (4, 14), 1)
+
+# instantiate Imputer
+imp = Imputer()
+
+for imp_method in imp_methods:
+    print 'Imputing with {}'.format(imp_method)
+    data = impute(x, imp, imp_method, params_dict)
+
+    # load respective scaler
+    scaler_path = os.path.join(scalers_folder,
+                               "{}_scaler".format(imp_method))
+
+    scaler_dict = pickle.load(open(scaler_path, "rb"))
+    for name, scaler in scaler_dict.items():
+        # scale and binarize, adding one col for missing value in all categ vars
+        data_scaled = np.copy(data)
+        data_scaled[:, params_dict['non_cat_cols']] = scaler.transform(
+            data[:, params_dict['non_cat_cols']].astype(float))
+        data_scaled_bin = imp.binarize_data(data_scaled,
+                                            params_dict['cat_cols'],
+                                            params_dict['miss_data_symbol'])
+        # convert to float
+        data_scaled_bin = data_scaled_bin.astype(float)
+
+        # add labels as last column
+        path = os.path.join(feats_test_folder,
+                            '{}_bin_scaled_test.np'.format(name))
+        data_scaled_bin = np.hstack((data_scaled_bin, labels))
+        print "\tSaving imputed data to {}".format(path)
+        data_scaled_bin.dump(path)
+    del data
+    del data_scaled
+    del data_scaled_bin
