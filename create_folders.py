47e3caff6071fdf13a9b47ba9646183534a12263
Rafael Valle <jrafaelvalle@gmail.com>
2016-03-05 00:15:38 +0000

    adding folder for imputed data

--- create_folders.py
+++ create_folders.py
@@ -2,7 +2,8 @@ import os
 from params import *
 
 folders = (feats_train_folder, labels_train_folder, feats_test_folder,
-labels_test_folder, perturb_folder, scalers_folder)
+labels_test_folder, perturb_folder, scalers_folder, imputed_folder) 
 for folder in folders:
     if not os.path.exists(folder):
+	print 'Creating {}'.format(folder)
 	os.makedirs(folder)
