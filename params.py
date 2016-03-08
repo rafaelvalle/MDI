eaf9f58f0d24b15a34d30c52c4e84472fce767eb
Rafael Valle <jrafaelvalle@gmail.com>
2016-03-08 00:01:25 -0800

    setting vars to float32

--- params.py
+++ params.py
@@ -16,8 +16,10 @@ params_dict['knn_summary_func'] = np.mean
 # folder paths
 feats_train_folder = "data/train/features/"
 labels_train_folder = "data/train/labels/"
+results_train_folder = "results/train"
 feats_test_folder = "data/test/features/"
 labels_test_folder = "data/test/labels/"
+results_test_folder = "results/test"
 perturb_folder = "data/perturbed/"
 scalers_folder = "data/scalers/"
 imputed_folder = "data/imputed"
@@ -25,7 +27,7 @@ imputed_folder = "data/imputed"
 # neural network parameters
 nnet_params_dict = {}
 nnet_params_dict['n_folds'] = 3
-nnet_params_dict['alphas'] = (1, 4, 9)
+nnet_params_dict['alphas'] = np.array([1, 4, 9], dtype=np.float32)
 nnet_params_dict['batch_sizes'] = (32, 512, 4096)
-nnet_params_dict['gammas'] = np.array([ 0.1 ,  0.01])
+nnet_params_dict['gammas'] = np.array([ 0.1 ,  0.01], dtype=np.float32)
 nnet_params_dict['max_epoch'] = 50
