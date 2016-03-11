b621dd4471ba048efc4e6e8e0e0ff0eee5f2f922
Rafael Valle <jrafaelvalle@gmail.com>
2016-03-10 16:54:31 -0800

    adding params for adadelta and lasagne

--- params.py
+++ params.py
@@ -1,10 +1,11 @@
 import numpy as np
 from scipy.stats import mode
+import lasagne
 
 # imputation parameters
 params_dict = {}
-# imp_methods = ['RandomReplace', 'Summary', 'RandomForest', 'PCA', 'KNN']
-imp_methods = ('KNN',)
+imp_methods = ('RandomReplace', 'Summary', 'RandomForest', 'PCA')
+# imp_methods = ('KNN',)
 params_dict['miss_data_symbol'] = '?'
 params_dict['miss_data_cond'] = lambda x: x == params_dict['miss_data_symbol']
 params_dict['cat_cols'] = (1, 3, 4, 5, 6, 7, 8, 12)
@@ -27,10 +28,16 @@ imputed_folder = "data/imputed"
 # neural network parameters
 nnet_params_dict = {}
 nnet_params_dict['n_folds'] = 3
-nnet_params_dict['alphas'] = np.array([1, 4, 9], dtype=np.float32)
-nnet_params_dict['batch_sizes'] = (32, 512, 4096)
+nnet_params_dict['depth'] = 3
+nnet_params_dict['alphas'] = np.array([0.1, 1, 4], dtype=np.float32)
+nnet_params_dict['batch_sizes'] = (64, 512, 4096)
 nnet_params_dict['gammas'] = np.array([0.1, 0.01], dtype=np.float32)
+nnet_params_dict['decay_rate'] = 0.95
 nnet_params_dict['max_epoch'] = 50
+nnet_params_dict['widths'] = (0, 1000, 2)
+nnet_params_dict['nonlins'] = (None, lasagne.nonlinearities.rectify,
+lasagne.nonlinearities.softmax)
+nnet_params_dict['drops'] = (0.2, 0.5, None)
 
 # random number seed
 rand_num_seed = 1
