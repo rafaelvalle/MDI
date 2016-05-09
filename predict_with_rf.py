785ce7796f7b9195974c16f840d5f21c323e69b2
Rafael Valle <jrafaelvalle@gmail.com>
2016-05-09 16:02:25 -0700

    adding prediction with raw data

--- predict_with_rf.py
+++ predict_with_rf.py
@@ -9,8 +9,10 @@ import cPickle as pkl
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.preprocessing import LabelEncoder
 from params import feats_train_folder, feats_test_folder
 from params import MODEL_DIRECTORY, RESULTS_PATH
+from params import adult_params
 
 
 dataname = 'adult'
@@ -62,7 +64,6 @@ if __name__ == '__main__':
 
             # Fit Tree Classifiers
             clfs = {
-
                     'DTC(max_depth=4)': 
                         DecisionTreeClassifier(max_depth=4),
                     'DTC(max_depth=8)': 
@@ -98,6 +99,56 @@ if __name__ == '__main__':
                 print("{} on {} error rate on test set: {}").format(
                     model_name, imputation_name, obj_val)
 
+    # predict using raw data
+    train_data = np.genfromtxt(
+        'data/adult-train-raw', delimiter=', ', dtype=object)
+    test_data = np.genfromtxt(
+        'data/adult-test-raw', delimiter=', ', dtype=object)
+    # remove redundant education-number
+    train_data = np.delete(train_data, 4, 1)
+    test_data = np.delete(test_data, 4, 1)
+
+    # create label encoders and replace data
+    for col in adult_params['cat_cols']:
+        encoder = LabelEncoder().fit(train_data[:, col])
+        train_data[:, col] = encoder.transform(train_data[:, col])
+        test_data[:, col] = encoder.transform(test_data[:, col])
+
+    clfs = {
+            'DTC(max_depth=4)': 
+                DecisionTreeClassifier(max_depth=4),
+            'DTC(max_depth=8)': 
+                DecisionTreeClassifier(max_depth=8),
+            'DTC(max_depth=16)': 
+                DecisionTreeClassifier(max_depth=16),
+            'DTC(max_depth=32)': 
+                DecisionTreeClassifier(max_depth=32),
+            'DTC(max_depth=256)': 
+                DecisionTreeClassifier(max_depth=256),
+            'DTC(max_depth=1024)': 
+                DecisionTreeClassifier(max_depth=1024),
+            'RFC(max_depth=4)': 
+                RandomForestClassifier(max_depth=4),
+            'RFC(max_depth=8)': 
+                RandomForestClassifier(max_depth=8),
+            'RFC(max_depth=16)': 
+                RandomForestClassifier(max_depth=16),
+            'RFC(max_depth=32)': 
+                RandomForestClassifier(max_depth=32),
+            'RFC(max_depth=256)': 
+                RandomForestClassifier(max_depth=256),
+            'RFC(max_depth=1024)': 
+                RandomForestClassifier(max_depth=1024)}
+
+    for model_name, clf in clfs.items():
+        clf.fit(train_data[:,:-1], train_data[:, -1])
+        y_test_hat = clf.predict(test_data[:,:-1])
+        obj_val = (sum(y_test_hat != test_data[:, -1]) /
+            float(len(test_data)))
+
+        model_preds[model_name+'RawData'] = obj_val
+        print("{} on {} error rate on test set: {}").format(
+            model_name, 'Raw_Data', obj_val)
     # dump dictionary
     pkl.dump(model_preds, open(
         os.path.join(RESULTS_PATH, 'trees_{}_results.np'.format(dataname)), 
